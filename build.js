29fb3ca924f0f1d3e6bd56ad8e07f0a41f7b3ffa
Yuriy Liskov <firsthash@gmail.com>
2017-06-28 01:15:41 +0300

    fix build

--- build.js
+++ build.js
@@ -7,5 +7,6 @@ const html = fs.readFileSync(path.join(__dirname, 'html/index.html'), 'utf8')
 const page = html
   .replace(/{{Title}}/, constants.SITE_TITLE)
   .replace(/{{State}}/, '')
-  .replace(/{{App}}/, '<main id="root"></main>');
+  .replace(/{{App}}/, '<main id="root"></main>')
+  .replace(/<script src="\/bundle.js"><\/script>/, '<script src="bundle.js"><\/script>');
 fs.writeFileSync(path.join(config.output.path, 'index.html'), page)
